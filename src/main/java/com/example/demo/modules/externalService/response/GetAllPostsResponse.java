73d32810739736cfc45c64953498d752e38b2725
harsh-chauhan-lumiq <harsh.chauhan@lumiq.ai>
2025-04-21 22:31:20 +0530

    feat(external-service): external service api call init

---
+++ src/main/java/com/example/demo/modules/externalService/response/GetAllPostsResponse.java
@@ -0,0 +1,20 @@
+package com.example.demo.modules.externalService.response;
+
+import java.util.List;
+
+import com.example.demo.modules.externalService.dto.PostsDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllPostsResponse {
+    private List<PostsDto> posts;
+}
